22f56a2a78655fe5644fe0a3f3543def5891b20f
Park-Dongheon <cdsded@naver.com>
2024-07-16 13:47:58 +0900

    day3

--- preamble.py
+++ preamble.py
@@ -2,11 +2,15 @@ from IPython.display import set_matplotlib_formats, display
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import koreanize_matplotlib
 import mglearn
 from cycler import cycler
+# pandas, numpy, matplotlib Setting
 
 set_matplotlib_formats('pdf', 'png')
-plt.rcParams['savefig.dpi'] = 300
+plt.rcParams['savefig.dpi'] = 300 # 해상도 크기
+# plt.rcParams['font.family'] = 'Malgun Gothic'
+# plt.rcParams['font.size'] = 9
 plt.rcParams['figure.dpi'] = 100
 plt.rcParams['image.cmap'] = "viridis"
 plt.rcParams['image.interpolation'] = "none"
@@ -18,7 +22,7 @@ plt.rc('axes', prop_cycle=(
     cycler('linestyle', ['-', '-', "--", (0, (3, 3)), (0, (1.5, 1.5))])))
 
 
-np.set_printoptions(precision=3, suppress=True)
+np.set_printoptions(precision=3, suppress=True) # 소숫점 이하 3자리까지 
 
 pd.set_option("display.max_columns", 8)
 pd.set_option('display.precision', 2)
