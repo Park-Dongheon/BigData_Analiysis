6d6b99269f96775af1aadd8eb9ea4a5a4c5c583a
Park-Dongheon <cdsded@naver.com>
2024-07-26 13:10:10 +0900

    day

--- nlp_version0.1/app.py
+++ nlp_version0.1/app.py
@@ -1,20 +1,23 @@
-from flask import Flask, render_template, request # 클래스
-from konlpy.tag import Okt
 import joblib
 import os
 import re
 
+from flask import Flask, render_template, request # 클래스
+from konlpy.tag import Okt
+
 app = Flask(__name__)
 app.debug = True
+
+okt = Okt()
 
 tfidf_vector = None
 model_lr = None
 
-okt = Okt()
 
 def tw_tokenzier(text):
     tokenzier_ko = okt.morphs(text)
     return tokenzier_ko
+
 
 def load_lr():
     # 전역변수
@@ -23,18 +26,27 @@ def load_lr():
     tfidf_vector = joblib.load(os.path.join(app.root_path, "model/tfidf_vect.pkl"))
     model_lr = joblib.load(os.path.join(app.root_path, "model/lr.pkl"))
 
-def lr_transform(review):
-    review = re.sub(r"\d+", " ", review)
-    test_matrix = tfidf_vector.transform([review])
-    return test_matrix
 
-@app.route("/predict", methods={"GET", "POST"})
+def lt_transform(review):
+    review = re.sub(r"\d+", " ", review)
+    tfidf_matrix = tfidf_vector.transform([review])
+    return tfidf_matrix
+
+
+@app.route("/predict", methods=["GET", "POST"])
 def nlp_predict():
     if request.method == "GET":
         return render_template("predict.html")
     else:
-        review = request.form["review"]
-        return render_template("predict_result.html", review=review)
+        _review = request.form["review"]
+        _review_matrix = lt_transform(_review)
+        _review_result = model_lr.predict(_review_matrix)[0]
+        _predict_result = "긍정" if _review_result else "부정"
+        result = {
+            "review":_review,
+            "review_result":_predict_result
+        }
+        return render_template("predict_result.html", result=result)
 
 
 @app.route("/")
@@ -45,7 +57,8 @@ def index():
     test_matrix = tfidf_vector.transform([test_str])
     result = model_lr.predict(test_matrix)
     print(result)
-    return render_template("index.html")
+    return render_template("index.html", result=result)
+
 
 if __name__ == "__main__":
     load_lr()
